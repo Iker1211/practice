fd35888f09d87a7b36aa84ba77735d31b3a4e58c
iker <velezlucasiker1@gmail.com>
2026-02-19 17:25:26 -0500

    add more vercel skills

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,7 @@
 // Aquí exportaremos tus componentes
 // export * from "./button";
+import { Button } from "./button"
+import { Card } from "./card"
+import { Code } from "./code"
+
+export { Button, Card, Code }
